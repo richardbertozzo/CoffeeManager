3ddc3329162524b4ede46e09ffa70e4449578062
richardjrb <richardjrbertozzo@gmail.com>
2017-07-26 19:24:31 -0300

    Configuração principal do projeto

--- app/src/main/java/richard/coffeemanager/CoffeeManagerActivity.java
+++ app/src/main/java/richard/coffeemanager/CoffeeManagerActivity.java
@@ -1,5 +1,6 @@
 package richard.coffeemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class CoffeeManagerActivity extends AppCompatActivity {
 
@@ -16,27 +21,27 @@ public class CoffeeManagerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee_manager);
 
+        String[] menus = listMenus();
+        final ListView listMenu = (ListView) findViewById(R.id.listMenus);
+        ArrayAdapter<String> arrayAdapterListMenu = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menus);
+        listMenu.setAdapter(arrayAdapterListMenu);
+        listMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent;
+                if (position == 0) {
+
+                } else if (position == 1) {
+                    intent = new Intent(CoffeeManagerActivity.this, ProductList.class);
+                    startActivity(intent);
+                } else if (position == 2) {
+
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_coffee_manager, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private String[] listMenus() {
+        return getResources().getStringArray(R.array.menus);
     }
 }
